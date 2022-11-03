ac928ef4fa593032ca5b96c6817de8a0186fa112
kasinathsg <108450699+kasinathsg@users.noreply.github.com>
2022-11-03 18:33:06 +0530

    Add files via upload

---
+++ win.js
@@ -0,0 +1,8 @@
+// let turns = localStorage.getItem("turns");
+// var scoreBoard = document.getElementById("score-board");
+// scoreBoard.innerHTML = turns;
+
+
+const backgroundSound = new Audio("./clap.mp3");
+backgroundSound.play();
+// backgroundSound.loop = true;
